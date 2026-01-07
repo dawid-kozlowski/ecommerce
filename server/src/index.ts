6fd9c062723c2bb42c482b78fb3f09b378ac11f2
Dawid <dknecksnap@gmail.com>
2026-01-07 18:28:59 +0100

    cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -28,7 +28,9 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL || '*'
+}));
 app.use(express.json());
 
 app.use('/products', productRoutes);
